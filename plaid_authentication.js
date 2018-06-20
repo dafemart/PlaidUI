fbaeb9e369be3482f71a84f70ff39fdc4ea16e98
Jing Yu <jingyu459510777@gmail.com>
2018-06-20 09:04:45 -0700

    hold

--- plaid_authentication.js
+++ plaid_authentication.js
@@ -1,45 +1,53 @@
-var AuthEndpoint =  "https://development.plaid.com/auth/get";
-var TransactionEndpoint = "https://development.plaid.com/transactions/get";
-var BalanceEndpoint = "https://development.plaid.com/accounts/balance/get";
-var DeleteEndpoint = "https://development.plaid.com/item/remove";
-
-function processRequest(EndPoint, body){
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-       if(this.readyState == 4 && this.status == 200){
-          return this.response;
-       }
-       else return false;
-    }
-    xhr.open("POST", EndPoint, true);
-    xhr.setRequestHeader('Content-type','application/json');
-    xhr.send(body);
-}
-
-function createAuthBody(){
-   var ClientId = document.getElementById("Authentication_clientId").value;
-   var secret = document.getElementById("Authentication_secret").value;
-   var token = document.getElementById().value;
-   var AuthBody = {};
-   authBody["client_id"] = ClientId;
-   authBody["secret"] = secret;
-   authBody["access_token"] = token;
-   return AuthBody;
-}
-
-function sendAuthRequest(){
-   var body = createAuthBody();
-   var authenticationData = JSON.parse(processRequest(AuthEndpoint, body));
-   console.log(authenticationData); //delete this one after test is finished
-}
-
 window.onload = function(){
   document.getElementById("Authentication_submit").
        addEventListener("click",sendAuthRequest,false);
 };
 
-function getAuthenticationData(){
 
+var AuthEndpoint =  "https://development.plaid.com/auth/get";
+var TransactionEndpoint = "https://development.plaid.com/transactions/get";
+var BalanceEndpoint = "https://development.plaid.com/accounts/balance/get";
+var DeleteEndpoint = "https://development.plaid.com/item/remove";
+
+
+function sendAuthRequest(){
+   var body = createAuthBody();
+  processRequest(AuthEndpoint, body);
+}
+
+function createAuthBody(){
+   var ClientId = document.getElementById("Authentication_clientId").value;
+   var secret = document.getElementById("Authentication_secret").value;
+   var token = document.getElementById("Authentication_token").value;
+   var AuthBody = {};
+    AuthBody["client_id"] = ClientId;
+    AuthBody["secret"] = secret;
+    AuthBody["access_token"] = token;
+   return AuthBody;
+}
+
+function processRequest(EndPoint, body){
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+       if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+        let authenticationData = JSON.parse(xhr.responseText);
+        getAuthenticationData(authenticationData);
+       }
+    }
+
+    xhr.open("POST", EndPoint, true);
+    xhr.setRequestHeader('Content-type','application/json');
+    xhr.send(body);
+}
+
+
+function getAuthenticationData(authenticationData){
+  if(authenticationData.message){
+         document.getElementById("authenticationMessage").innerHTML = '<span class="label label-danger label-center">Incorrect input.</span>';
+  }
+  else{
   //Clean up data in Id displayData
   document.body.appendChild(document.createElement('pre')).innerHTML = JSON.stringify(authenticationDatadata,null,4);
+    }
 }
